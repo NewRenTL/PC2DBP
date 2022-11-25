4d94d7a6c8ee012833ec251591c6cabcb50e36e3
S1erpe <diego.bustamante@utec.edu.pe>
2022-11-24 19:58:26 -0500

    InicarEnvioTODO

---
+++ src/components/CityComponent.vue
@@ -0,0 +1,55 @@
+<script>
+export default {
+  name: "CityComponent",
+  props: [
+    "key",
+    "name",
+    "country_name",
+    'country_code',
+    'state_name',
+    'state_code',
+    'latitude',
+    'longitude'
+    //TODO: crear una propiedad para determinar si al componente se le puede hacer click
+  ],
+};
+</script>
+
+<template>
+  <!--TODO: Utilizar la propiedad creada para controlar el click. Cuando el componente es "clickeable", redirigir a la pagina del pais.
+  Mira las rutas para ver como redirigir al usuario.-->
+  <div class = "country">
+  <!--TODO: Poblar el HTML con las propiedades. Usar https://countryflagsapi.com/png/${code}-->
+  <img
+      width="200"
+      height="200"
+      src ="../assets/city.jpg"
+    />
+  <h1>Pais: {{country_name}} {{country_code}}</h1><br>
+  <p>Estado : {{state_name}} {{state_code}}</p><br>
+  <p>Latitude : {{latitude}}</p><br>
+  <p>Longitude : {{longitude}}</p><br>
+  </div>
+</template>
+
+<style scoped>
+.country {
+  padding: 2px;
+  white-space: nowrap;
+  width: 350px;
+  height: 350px;
+  border-style: solid;
+  border-radius: 5px;
+  border-color: grey;
+}
+
+.country-hover:hover {
+  background-color: rgb(49, 49, 49);
+  cursor: pointer;
+}
+
+h1 {
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+</style>
---
+++ src/views/CapitalsGameView.vue
@@ -0,0 +1,74 @@
+<script>
+import countries from "/src/datasets/countries.json";
+import CountryComponent from "../components/CountryComponent.vue";
+
+export default {
+  name: "CapitalsGameView",
+  components: {
+    CountryComponent,
+  },
+  data() {
+    return {
+      country: {},
+      capital: "",
+      puntaje: 0,
+      val : false,
+
+    };
+  },
+  created() {
+    this.country = countries[Math.floor(Math.random() * countries.length)];
+  },
+  methods: {
+    setCapital(e) {
+      //TODO: implementar. fija la capital del input.
+      this.capital = e.target.value;
+    },
+    adivinarCapital(e) {
+      //TODO: implementar. verifica si la adivinanza es correcta.
+      if(e.target.value === this.capital) {
+        this.val = true;
+      }
+      
+    },
+  },
+};
+</script>
+
+<template>
+  <h1>Puntaje: {{ puntaje }}</h1>
+  <div class="countries">
+    <!-- TODO: usar los metodos definidos arriba dentro del input para llenar el estado de la capital a adivinar-->
+    <input
+      placeholder="Adivina la capital"
+    />
+    <button @click="adivinarCapital">Adivina!</button>
+    <div class="countries-container">
+      <!--TODO: CREAR EL COMPONENTE PARA VISUALIZAR EL PAIS-->
+      <img src = "http://www.countryflagsapi.com/png/{{country.iso3}}" >
+      <p>Moneda: {{country.currency_name}} {{country.currency}}</p>
+      <br><br>
+      <p>Region {{country.region}}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  .countries {
+    align-items: center;
+  }
+
+  input {
+    line-height: 2em;
+  }
+
+  .countries-container {
+    margin-left: 35%;
+    align-items: center;
+    overflow-y: auto;
+    vertical-align: middle;
+    padding: 10px;
+  }
+}
+</style>
